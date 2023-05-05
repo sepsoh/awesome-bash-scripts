749137b6a4ba204c0ae3b5eb439e0bf03b8dba96
Sepehr <87861266+sepsoh@users.noreply.github.com>
2023-05-06 01:06:08 +0330

    Create shecan.sh
    initial commit

---
+++ Shecan DNS Changer/shecan.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# See More github.com/sepsoh/awesome-bash-scripts
+
+
+# Check if the user passed in the "start" argument
+if [ "$1" = "start" ]; then
+  # Backup the original resolv.conf file
+  sudo cp /etc/resolv.conf /etc/resolv.conf.shecan.bak
+  # Add the two DNS servers to resolv.conf
+  sudo sh -c 'echo "nameserver 178.22.122.100" > /etc/resolv.conf'
+  sudo sh -c 'echo "nameserver 185.51.200.2" >> /etc/resolv.conf'
+  # Restart the NetworkManager service
+  sudo systemctl restart NetworkManager
+  echo "Successfully shecan started."
+elif [ "$1" = "stop" ]; then
+  # Restore the original resolv.conf file
+  sudo cp /etc/resolv.conf.shecan.bak /etc/resolv.conf
+  echo "Successfully shecan stopped."
+  # Restart the NetworkManager service
+  sudo systemctl restart NetworkManager
+elif [ -z "$1" ]; then
+  # No argument was passed, print help message
+  echo "Usage: sudo ./shecan.sh [start/stop]"
+else
+  # Invalid argument was passed, print help message
+  echo "Invalid argument. Usage: sudo ./shecan.sh [start/stop]"
+fi
