18340ff5948b69e3442d039d9f68fa51f474e264
alifazl3 <fazlolahiali@gmail.com>
2023-05-18 01:25:06 +0330

    fixed code for mac os

--- Shecan DNS Changer/shecan.sh
+++ Shecan DNS Changer/shecan.sh
@@ -1,14 +1,70 @@
 #!/bin/bash
 # See More github.com/sepsoh/awesome-bash-scripts
 
+# init vars
+DNS1=178.22.122.100
+DNS2=185.51.200.2
+declare -a old_dns
+
+#  check system os
+if [[ $(uname) == "Darwin" ]]; then
+    SYSTEM_OS=Mac
+    echo "This is a Mac."
+else
+    SYSTEM_OS=Linux
+    echo "This is not a Mac."
+fi
+
+
+
+#--------------------
+#--------mac---------
+#--------------------
+
+
+if [ "$SYSTEM_OS" = "Mac" ]; then
+  SERVICE=Wi-Fi
+  case "$1" in
+    start)
+      echo "Setting DNS..."
+      IFS=' ' read -r -a old_dns <<< "$(networksetup -getdnsservers $SERVICE)"
+      networksetup -setdnsservers $SERVICE $DNS1 $DNS2
+      sudo dscacheutil -flushcache
+      sudo killall -HUP mDNSResponder
+      echo "cache flushed"
+      echo "Successfully shecan started."
+    ;;
+    stop)
+        echo "Resetting DNS..."
+        if [ ${#old_dns[@]} -eq 0 ]; then
+            networksetup -setdnsservers $SERVICE empty
+        else
+            networksetup -setdnsservers $SERVICE "${old_dns[@]}"
+        fi
+        sudo dscacheutil -flushcache
+        sudo killall -HUP mDNSResponder
+        echo "cache flushed"
+        echo "Successfully shecan stopped."
+      ;;
+    *)
+        echo "Invalid argument. Usage: sudo $0 {start|stop}"
+  esac
+  exit
+fi
+
+
+#--------------------
+#-------linux--------
+#--------------------
+
 
 # Check if the user passed in the "start" argument
 if [ "$1" = "start" ]; then
   # Backup the original resolv.conf file
   sudo cp /etc/resolv.conf /etc/resolv.conf.shecan.bak
   # Add the two DNS servers to resolv.conf
-  echo "nameserver 178.22.122.100" | sudo tee /etc/resolv.conf > /dev/null
-  echo "nameserver 185.51.200.2" | sudo tee -a /etc/resolv.conf > /dev/null
+  echo "nameserver $DNS1" | sudo tee /etc/resolv.conf > /dev/null
+  echo "nameserver $DNS2" | sudo tee -a /etc/resolv.conf > /dev/null
   echo "Successfully shecan started."
 elif [ "$1" = "stop" ]; then
   # Restore the original resolv.conf file
