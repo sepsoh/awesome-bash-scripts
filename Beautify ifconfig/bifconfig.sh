96f6e3644c380ded10c35063973e44500eb060c3
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-12-14 16:15:06 +0330

    update: resize interface length to two tabs

--- Beautify ifconfig/bifconfig.sh
+++ Beautify ifconfig/bifconfig.sh
@@ -5,12 +5,12 @@ ips=$(ifconfig  | grep "^\s" |  sed 's/ \{1,\}/\n/' | grep '^inet ' | cut -f2 -d
 netmasks=$(ifconfig  | grep "^\s" |  sed 's/ \{1,\}/\n/' | grep '^inet ' | cut -f5 -d' ') # get netmask
 flags=$(ifconfig | grep "^[[:alpha:]]" | sed 's/\s//' | cut -f2 -d: | sed 's/  / /g' | awk '{print $1}' | cut -f2 -d=) # get flags
 
-printf "%-10s %-15s %-15s %-40s\n" "interface" "IP" "netmask" "flags"
+printf "%-16s %-15s %-15s %-40s\n" "interface" "IP" "netmask" "flags"
 for n in $(seq 1 $number_of_interfaces)
 do
     interface=$(echo $interfaces | sed 's/\s/\n/g' | head -$n | tail -1)
     ip=$(echo $ips | sed 's/\s/\n/g' | head -$n | tail -1)
     netmask=$(echo $netmasks | sed 's/\s/\n/g' | head -$n | tail -1)
     flag=$(echo $flags | sed 's/\s/\n/g' | head -$n | tail -1)
-    printf "%-10s %-15s %-15s %-40s\n" "$interface" "$ip" "$netmask" "$flag"
+    printf "%-16s %-15s %-15s %-40s\n" "$interface" "$ip" "$netmask" "$flag"
 done
