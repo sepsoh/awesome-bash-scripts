baa3a613f497f13e872eea4246039eaba9493d6e
Milad Rasouli <milad75rasouli@gmail.com>
2024-03-12 12:43:17 +0330

    feat: guide user if input file does not exist

--- Scripts/Share File/share-file.sh
+++ Scripts/Share File/share-file.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # See More github.com/sepsoh/awesome-bash-scripts
 
-curl -F "file=@$1" 0x0.st
+
+if [ -z ${1} ]; then
+    echo -e "Enter the input file!\ncurl -F "file=@\<your_file\>" 0x0.st"
+else
+    curl -F "file=@$1" 0x0.st
+fi
