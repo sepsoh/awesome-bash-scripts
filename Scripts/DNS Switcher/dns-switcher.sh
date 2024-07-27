dacc858ed084de46050bddcdb5de8173c501d963
MrMeshky <mr.meshky@gmail.com>
2024-07-27 11:01:21 +0330

    feat: add about to menu

--- Scripts/DNS Switcher/dns-switcher.sh
+++ Scripts/DNS Switcher/dns-switcher.sh
@@ -22,9 +22,9 @@ CLOUD_FLARE_DNS2="1.0.0.1"
 RADAR_DNS1="10.202.10.10"
 RADAR_DNS2="10.202.10.11"
 
-options=("Shecan" "Electro" "Begzar" "Google" "403" "CloudFlare" "Radar" "Default")
-dns1_list=($SHECAN_DNS1 $ELECTRO_DNS1 $BEGZAR_DNS1 $GOOGLE_DNS1 $ONLINE_403_DNS1 $CLOUD_FLARE_DNS1 $RADAR_DNS1 $DEFALT_DNS1)
-dns2_list=($SHECAN_DNS2 $ELECTRO_DNS2 $BEGZAR_DNS2 $GOOGLE_DNS2 $ONLINE_403_DNS2 $CLOUD_FLARE_DNS2 $RADAR_DNS2 $DEFALT_DNS1)
+options=("Default" "Shecan" "Electro" "Begzar" "Google" "403" "CloudFlare" "Radar" "About")
+dns1_list=($DEFALT_DNS1 $SHECAN_DNS1 $ELECTRO_DNS1 $BEGZAR_DNS1 $GOOGLE_DNS1 $ONLINE_403_DNS1 $CLOUD_FLARE_DNS1 $RADAR_DNS1 "")
+dns2_list=($DEFALT_DNS1 $SHECAN_DNS2 $ELECTRO_DNS2 $BEGZAR_DNS2 $GOOGLE_DNS2 $ONLINE_403_DNS2 $CLOUD_FLARE_DNS2 $RADAR_DNS2 "")
 
 get_current_dns_name() {
     current_dns1=$(grep -m 1 "nameserver" /etc/resolv.conf | awk '{print $2}')
@@ -42,7 +42,7 @@ print_menu() {
     clear
     current_dns_name=$(get_current_dns_name)
     echo -e "Current DNS Configuration: ${RED}$current_dns_name${NC}"
-    echo -e "\nWhich DNS do you want to use?"
+    echo -e "\nWhich DNS or option do you want to use?"
     for i in ${!options[@]}; do
         if [ $i -eq $selected ]; then
             tput setaf 4; tput bold
@@ -52,6 +52,17 @@ print_menu() {
             echo "   ${options[$i]}"
         fi
     done
+}
+
+show_about() {
+    clear
+    echo -e "${BLUE}About This Script${NC}"
+    echo -e "This script allows you to change your DNS settings quickly and easily."
+    echo -e "You can choose from several predefined DNS servers or add your own."
+    echo -e "For more information and to access other scripts, visit:"
+    echo -e "${YELLOW}https://github.com/Mr-Meshky/dns-switcher${NC}"
+    echo -e "\nPress any key to return to the menu..."
+    read -n1
 }
 
 selected=0
@@ -74,17 +85,22 @@ while true; do
             fi
             ;;
         "") 
-            DNS1=${dns1_list[$selected]}
-            DNS2=${dns2_list[$selected]}
-            break
+            if [ "${options[$selected]}" == "About" ]; then
+                show_about
+                print_menu
+            else
+                DNS1=${dns1_list[$selected]}
+                DNS2=${dns2_list[$selected]}
+                break
+            fi
             ;;
     esac
     print_menu
 done
 
-echo "nameserver $DNS1" | sudo tee /etc/resolv.conf > /dev/null
-echo "nameserver $DNS2" | sudo tee -a /etc/resolv.conf > /dev/null
-cat /etc/resolv.conf
-
-echo -e "${GREEN}Setting DNS to ${options[$selected]} is done successfully${NC}\n"
-echo -e "${YELLOW}Developed by MrMeshky.ir with love${NC}"
+if [ -n "$DNS1" ]; then
+    echo "nameserver $DNS1" | sudo tee /etc/resolv.conf > /dev/null
+    echo "nameserver $DNS2" | sudo tee -a /etc/resolv.conf > /dev/null
+    cat /etc/resolv.conf
+    echo -e "${GREEN}Setting DNS to ${options[$selected]} is done successfully${NC}\n"
+fi
