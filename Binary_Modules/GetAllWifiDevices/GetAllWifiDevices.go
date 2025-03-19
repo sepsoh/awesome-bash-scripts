5e9f8dba80f9f21e6a6de3290cc051297ed9cd23
Ali Ghaffarian <alighaffarian9@gmail.com>
2025-03-19 18:45:36 +0330

    add GetAllWifiDevices

---
+++ Binary_Modules/GetAllWifiDevices/GetAllWifiDevices.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Wifx/gonetworkmanager/v3"
+)
+
+func if_err_log_and_die(err error){
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func main (){
+	nm, err := gonetworkmanager.NewNetworkManager()
+	if_err_log_and_die(err)
+
+	devices, err := nm.GetAllDevices()
+	if_err_log_and_die(err)
+
+	for i := 0; i < len(devices); i++{
+		device_type, err := devices[i].GetPropertyDeviceType()
+		if_err_log_and_die(err)
+
+		if device_type == gonetworkmanager.NmDeviceTypeWifi{
+			fmt.Println(devices[i].GetPath())
+		}
+		
+	}
+
+}
---
+++ Binary_Modules/GetAllWifiDevices/go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.19
+
+require (
+	github.com/Wifx/gonetworkmanager/v3 v3.0.0 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+)
