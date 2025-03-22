f5fe5c614ceeb7cdc0033b64d5d550841e584be5
Ali Ghaffarian <alighaffarian9@gmail.com>
2025-03-22 22:49:07 +0330

    add option for detailed output

--- Binary_Modules/GetAllWifiDevices/GetAllWifiDevices.go
+++ Binary_Modules/GetAllWifiDevices/GetAllWifiDevices.go
@@ -4,7 +4,15 @@ import (
 	"fmt"
 	"os"
 	"github.com/Wifx/gonetworkmanager/v3"
+	"github.com/alexflint/go-arg"
 )
+
+const (
+	detail_delim = ","
+)
+var args struct {
+	Detailed bool
+}
 
 func if_err_log_and_die(err error){
 	if err != nil {
@@ -13,7 +21,24 @@ func if_err_log_and_die(err error){
 	}
 }
 
+func output_device_based_on_args(device gonetworkmanager.Device) error {
+	print_string := string(device.GetPath())
+
+	if args.Detailed {
+		ifname, err := device.GetPropertyInterface()
+		if err != nil {
+			return err
+		}
+		print_string += detail_delim
+		print_string += ifname
+	}
+	fmt.Println(print_string)
+	return nil
+}
+
 func main (){
+	arg.MustParse(&args)
+
 	nm, err := gonetworkmanager.NewNetworkManager()
 	if_err_log_and_die(err)
 
@@ -25,7 +50,7 @@ func main (){
 		if_err_log_and_die(err)
 
 		if device_type == gonetworkmanager.NmDeviceTypeWifi{
-			fmt.Println(devices[i].GetPath())
+			_ = output_device_based_on_args(devices[i])
 		}	
 	}
 }
